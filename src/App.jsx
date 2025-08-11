d7cdd10c974b9ffa950a71886f92624143a7f6d1
Peera <peerawit.sut@gmail.com>
2025-08-11 17:15:44 +0700

    landing-page-v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Button } from './components/ui/button';
 import Login from './views/login';
+import LandingPage from './views/landingPage';
 
 
 const App = () => {
   return (
     <div>
-      <Login/>
+      <LandingPage/>
     </div>
   )
 }
