f393bf80ff63952dc7c534d3227c579995b10754
Sarawoot-K <jays.sara27@gmail.com>
2025-08-11 15:50:36 +0700

    Nav:add-logo nav-style

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -14,45 +14,56 @@ import { Link, Outlet } from "react-router-dom";
 const Navber = () => {
   return (
     <div>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuLink asChild>
-              <Link to="/">Home</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink asChild>
-              <Link to="/pricing">Pricing</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink asChild>
-              <Link to="/tracking">Tracking</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink asChild>
-              <Link to="/bookService">Book Service</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link to="/myOder">My Oder</Link>
-          </NavigationMenuLink>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>
+      <div className="flex justify-between">
+        <div>
+          <img
+            src="/src/image/Whale wash1.png"
+            alt=""
+            className="w-12 rounded-4xl m-2"
+          />
+        </div>
+        <div className=" flex justify-end ">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link to="/">Home</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link to="/pricing">Pricing</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link to="/tracking">Tracking</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link to="/bookService">Book Service</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
               <NavigationMenuLink asChild>
-                <Link to="/profile">Profile</Link>
+                <Link to="/myOder">My Oder</Link>
               </NavigationMenuLink>
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  <NavigationMenuLink asChild>
+                    <Link to="/profile">Profile</Link>
+                  </NavigationMenuLink>
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink>Link</NavigationMenuLink>
+                  <NavigationMenuLink>Link</NavigationMenuLink>
+                  <NavigationMenuLink>Link</NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </div>
       <div className="p-6 w-full max-w-6xl">
         <Outlet />
       </div>
