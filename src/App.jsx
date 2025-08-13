6bdf3ccc102c05e5879ed7049c8ba0a365a5a13f
watcharap <watchara.paramee@gmail.com>
2025-08-13 10:03:00 +0700

    add view dashboardview

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
-import { Button } from './components/ui/button';
-import Login from './views/login';
+import React from "react";
+import Navber from "./components/Navber";
+import Profile from "./views/Profile";
+import Home from "./views/Home";
+import Pricing from "./views/Pricing";
+import Tracking from "./views/Tracking";
+import BookService from "./views/BookService";
+import MyOrder from "./views/MyOrder";
+import Login from "./views/login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Navber />,
+    errorElement: (
+      <div>
+        <h1 className="text-2xl font-bold mb-4 object-center">
+          404 - page Not Found
+        </h1>
+      </div>
+    ),
+    children: [
+      { path: "/home", element: <Home /> },
+      { path: "/home/pricing", element: <Pricing /> },
+      { path: "/home/tracking", element: <Tracking /> },
+      { path: "/home/bookService", element: <BookService /> },
+      { path: "/home/myOder", element: <MyOrder /> },
+      { path: "/home/profile", element: <Profile /> },
+    ],
+  },
+]);
 
 const App = () => {
-  return (
-    <div>
-      <Login/>
-    </div>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
