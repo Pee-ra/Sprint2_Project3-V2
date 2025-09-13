087e6474392a9972f9e6ed4eb1026b258aef1c80
Peera <peerawit.sut@gmail.com>
2025-09-13 22:03:32 +0700

    debug v2 adminCustomer url

--- src/views/AdminCustomers.jsx
+++ src/views/AdminCustomers.jsx
@@ -33,7 +33,7 @@ export function AdminCustomers() {
   useEffect(() => {
     (async () => {
       try {
-        const res = await axios.get(`${import.meta.env.VITE_API_URL || "http://localhost:5001"}/adminPage/users/${userId}`, { withCredentials: true });
+        const res = await axios.get(`${import.meta.env.VITE_API_URL || "http://localhost:5001"}/adminPage/users`, { withCredentials: true });
         const users = res.data.users;
 
         const mapUsers = users.map((user) => ({
